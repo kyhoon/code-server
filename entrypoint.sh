7945cf11c5119952e4017ef132b97007e1fe5a67
kyhoon <yh01dlx@gmail.com>
2019-10-25 00:30:10 +0900

    Add sudo to cp in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Copy SSH keys from mounted volume
-cp -R /.ssh /home/coder/.ssh
+sudo cp -R /.ssh /home/coder/.ssh
 sudo chmod 700 /home/coder/.ssh
 sudo chmod 644 /home/coder/.ssh/id_rsa.pub
 sudo chmod 400 /home/coder/.ssh/id_rsa
